682aa6ec76633007350fe501d0f9cf6e6a71cfc5
mahiagarwal2772-stack <mahiagarwal2772@gmail.com>
2026-05-30 21:09:17 +0530

    Add files via upload

---
+++ Day 3/d31.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,c=0;
+    printf("Enter a number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        printf("Prime number");
+    }
+    else
+    {
+        printf("Not a prime number");
+    }
+    return 0;
+}
